17c424dbfefb070641ad8f251819c6eaa6dfdaf8
殷豪 <jjyyg1123@gmail.com>
2018-11-05 18:02:29 +0800

    fix: user_profile新增員工到職日並修改欄位string長度

--- database/migrations/2018_11_04_091400_create_user_profile_table.php
+++ database/migrations/2018_11_04_091400_create_user_profile_table.php
@@ -17,9 +17,10 @@ class CreateUserProfileTable extends Migration
             Schema::create('user_profile', function (Blueprint $table) {
                 $table->increments('id');
                 $table->unsignedInteger('user_id');
-                $table->string('name')->nullable();
+                $table->string('name', 30)->nullable();
                 $table->string('staff_code')->nullable()->comment('員工編號');
-                $table->string('phone_number')->nullable();
+                $table->string('phone_number', 20)->nullable();
+                $table->timestamp('on_board_date')->nullable()->comment('到職日');
                 $table->timestamps();
 
                 $table->foreign('user_id')
