90ea8ecf425725902e73167ac8751092a1594cdc
殷豪 <jjyyg1123@gmail.com>
2018-11-15 14:52:45 +0800

    fix: 限制經緯度欄位長度

--- database/migrations/2018_11_14_105813_create_locations_table.php
+++ database/migrations/2018_11_14_105813_create_locations_table.php
@@ -19,8 +19,8 @@ class CreateLocationsTable extends Migration
                 $table->unsignedInteger('company_id');
                 $table->unsignedInteger('department_id');
                 $table->string('name', 50)->nullable();
-                $table->string('latitude')->nullable();
-                $table->string('longitude')->nullable();
+                $table->string('latitude', 50)->nullable();
+                $table->string('longitude', 50)->nullable();
                 $table->string('address')->nullable();
                 $table->unsignedInteger('legal_distance')->default(100)->comment('允許打卡距離(公尺)');
                 $table->unsignedInteger('created_at')->nullable();
