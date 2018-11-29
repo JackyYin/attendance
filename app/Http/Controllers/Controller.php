6c3cc7a4c8cb167112656e852eb26e27bef5e03f
殷豪 <jjyyg1123@gmail.com>
2018-11-29 18:32:25 +0800

    refactor: 調整app/Http/Controllers/Controller , 拿掉多餘的code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -22,36 +22,17 @@ class Controller extends BaseController
 
     public function user()
     {
-        try {
-            $model = $this->payload()->get('model');
-            $id = $this->payload()->get('sub');
-        } catch (\Exception $e) {
-            dump($e);
-            return NULL;
-        }
-
-        if (!$model || !$id) {
-            return NULL;
-        }
+        $model = $this->jwt->getPayload()->get('model');
+        $id = $this->jwt->getPayload()->get('sub');
 
         return $model::find($id);
-    }
-
-    /**
-     * Get the raw Payload instance.
-     *
-     * @return \Tymon\JWTAuth\Payload
-     */
-    public function payload()
-    {
-        return $this->jwt->getPayload();
     }
 
     public function checkAgent($userToken, Agent $agent) : bool
     {
         $this->jwt->setToken($userToken);
 
-        if ($this->payload()->get('agent_id') != $agent->id) {
+        if ($this->jwt->getPayload()->get('agent_id') != $agent->id) {
             return false;
         }
 
