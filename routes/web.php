564a7846e724ff653caa424b370c1814242e8e10
殷豪 <jjyyg1123@gmail.com>
2018-12-02 21:41:51 +0800

    fix: route resouces 改成複數

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,18 @@ $router->group(['middleware' => 'custom-jwt:company'], function () use ($router)
     $router->get('/version', function () use ($router) {
         return $router->app->version();
     });
-    $router->group(['prefix' => 'company', 'as' => 'company'], function () use ($router) {
+    $router->group(['prefix' => 'companies', 'as' => 'company'], function () use ($router) {
         $router->get('/me', ['as' => 'me', 'uses' => 'Company\AuthController@me']);
         $router->post('/logout', ['as' => 'logout', 'uses' => 'Company\AuthController@logout']);
         $router->post('/refresh', ['as' => 'refresh', 'uses' => 'Company\AuthController@refresh']);
     });
-    $router->group(['prefix' => 'user', 'as' => 'user'], function () use ($router) {
+    $router->group(['prefix' => 'users', 'as' => 'user'], function () use ($router) {
         $router->get('/create', ['as' => 'create', 'uses' => 'UserController@create']);
         $router->post('/', ['as' => 'store', 'uses' => 'UserController@store']);
     });
 });
 
-$router->group(['prefix' => 'company', 'as' => 'company'], function () use ($router) {
+$router->group(['prefix' => 'companies', 'as' => 'company'], function () use ($router) {
     $router->post('/', ['as' => 'store', 'uses' => 'CompanyController@store']);
     $router->post('/authenticate', ['as' => 'authenticate', 'uses' => 'Company\AuthController@authenticate']);
 });
