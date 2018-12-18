8daa213a996e5bf78f78c7c0617f7edc20e657f0
殷豪 <jjyyg1123@gmail.com>
2018-12-18 21:11:41 +0800

    fix: route()產生的url可以為http/https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->share('session', '');
-        if(env('FORCE_HTTPS')) {
-            URL::forceScheme('https');
-        }
+        $this->app['request']->server->set('HTTPS', env('FORCE_HTTPS'));
     }
 
     /**
